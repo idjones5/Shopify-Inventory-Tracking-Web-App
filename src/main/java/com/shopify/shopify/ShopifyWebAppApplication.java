546150da8c00d1f2c17429ec56bb9e2f3c62e5b3
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-05-18 17:30:53 -0700

    Initial Commit.

---
+++ src/main/java/com/shopify/shopify/ShopifyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.shopify.shopify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyWebAppApplication.class, args);
+	}
+
+}
